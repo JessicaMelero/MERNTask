d10606dfea4557f5d3773923b8d92b316dcef64e
jessica <jessicameleromartinez@gmail.com>
2020-04-11 21:36:35 +0200

    Listando proyectos creados

---
+++ src/components/proyectos/ListadoProyectos.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Proyecto from './Proyecto';
+
+const ListadoProyectos = ({}) => {
+
+  const proyectos = [
+    {nombre: 'Tienda Virtual'},
+    {nombre: 'Diseño sitio web'},
+    {nombre: 'Intranet'},
+
+  ]
+
+  return (
+    <ul className="listado-proyectos">
+      {proyectos.map(proyecto=>(
+        <Proyecto
+          proyecto={proyecto}
+          />
+      ))}
+    </ul>
+
+  );
+};
+
+export default ListadoProyectos;
