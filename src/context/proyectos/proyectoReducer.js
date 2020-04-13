81aac0ac024df462b4c1bfa6aa5f64fa6705917a
jessica <jessicameleromartinez@gmail.com>
2020-04-13 12:35:06 +0200

    Colocar proyecto activo

--- src/context/proyectos/proyectoReducer.js
+++ src/context/proyectos/proyectoReducer.js
@@ -2,7 +2,8 @@ import {
   FORMULARIO_PROYECTO,
   OBTENER_PROYECTO,
   AGREGAR_PROYECTO,
-  VALIDAR_FORMULARIO
+  VALIDAR_FORMULARIO,
+  PROYECTO_ACTUAL
 } from '../../types';
 
 export default (state, action) => {
@@ -24,11 +25,16 @@ export default (state, action) => {
           formulario: false,
           errorformulario: false
         }
-        case VALIDAR_FORMULARIO:
-          return {
-            ...state,
-            errorformulario: true
-          }
+      case VALIDAR_FORMULARIO:
+        return {
+          ...state,
+          errorformulario: true
+      }
+      case PROYECTO_ACTUAL:
+        return {
+          ...state,
+          proyecto: state.proyectos.filter(proyecto=> proyecto.id === action.payload)
+      }
     default:
       return state;
 
