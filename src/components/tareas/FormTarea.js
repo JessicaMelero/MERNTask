456f684fa1d3c27bed561cd8635ad78597023132
jessica <jessicameleromartinez@gmail.com>
2020-04-13 18:27:13 +0200

    Dispatch para nuevas tareas

--- src/components/tareas/FormTarea.js
+++ src/components/tareas/FormTarea.js
@@ -13,9 +13,23 @@ const FormTarea = () => {
   // Array destructuring para extraer el proyecto proyectoActual
   const [proyectoActual] = proyecto;
 
+  const onSubmit = e => {
+    e.preventDefault();
+
+    // validar
+
+    // pasar la validacion
+
+    // agregar nueva tarea al state de tareas
+
+    // reiniciar el form
+  }
+
   return(
     <div className="formulario">
-      <form>
+      <form
+        onSubmit={onSubmit}
+        >
         <div className="contenedor-input">
           <input
             type="text"
