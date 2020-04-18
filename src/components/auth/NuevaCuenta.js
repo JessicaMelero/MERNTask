913813aa47798711dbac2e2a9f7d4bd654716c00
jessica <jessicameleromartinez@gmail.com>
2020-04-18 16:02:23 +0200

    autenticar y redirigir al usuario

--- src/components/auth/NuevaCuenta.js
+++ src/components/auth/NuevaCuenta.js
@@ -1,15 +1,25 @@
-import React, {useState, useContext} from 'react';
+import React, {useState, useContext, useEffect} from 'react';
 import {Link} from 'react-router-dom';
 import AlertaContext from '../../context/alertas/alertaContext';
 import AuthContext from '../../context/autenticacion/authContext';
 
-const NuevaCuenta = ({}) => {
+const NuevaCuenta = (props) => {
       // Extraer lso valores de context
       const alertaContext = useContext(AlertaContext);
       const {alerta, mostrarAlerta} = alertaContext;
 
       const authContext = useContext(AuthContext);
-      const {registrarUsuario} = authContext;
+      const {mensaje, autenticado, registrarUsuario} = authContext;
+
+      // En caso de usuario autenticado, regitrado o duplicado
+      useEffect(() => {
+        if (autenticado) {
+          props.history.push('/proyectos');
+        }
+        if (mensaje) {
+          mostrarAlerta(mensaje.msg, mensaje.categoria);
+        }
+      }, [mensaje, autenticado, props.history]);
 
       // state para iniciar sesion
       const [usuario, guardarUsuario] = useState({
